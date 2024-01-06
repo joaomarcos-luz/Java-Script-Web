a9c1b3fde67456cb775268602f2d071536373a03
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-01-06 14:29:31 -0300

    melhorias  casa noturna

---
+++ exercicios/ex-029/script.js
@@ -0,0 +1,9 @@
+function verificarCadastrar(){
+
+    let res = document.getElementById('resp')
+    let nom = document.getElementById('nome')
+    let cpf = document.getElementById('cpf')
+    let nac = document.getElementById('nacimento')
+
+    res.innerHTML = '<p>Olá</p>';
+}
