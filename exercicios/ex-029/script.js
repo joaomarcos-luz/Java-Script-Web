9d9abae737538d0018e57ee1b7eee8007b87638b
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-01-08 07:46:24 -0300

    modificações

--- exercicios/ex-029/script.js
+++ exercicios/ex-029/script.js
@@ -1,9 +1,23 @@
-function verificarCadastrar(){
-
+function verificarCadastrar() {
     let res = document.getElementById('resp')
     let nom = document.getElementById('nome')
     let cpf = document.getElementById('cpf')
     let nac = document.getElementById('nacimento')
 
-    res.innerHTML = '<p>Olá</p>';
+    let nome = nom.
+    let insNom = nome.length > 0
+
+    let inCpf = cpf.value.trim()
+    let insCpf = inCpf.length === 11
+
+    let nacimento = nac.value.trim()
+    let insNac = nacimento.length === 8
+
+    if (insNom && insCpf && insNac) {
+        res.innerHTML = '<h2>Cadastro Realizado.</h2>'
+    } else {
+        res.innerHTML = '<h2>[Err] Verifique os dados preenchidos.</h2>'
+    }
 }
+
+    
